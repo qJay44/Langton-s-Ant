b0f1736eb44a5d07e731100d44b9b2a9eadde40f
qJay44 <m70155302@yandex.ru>
2023-02-05 01:24:19 +0300

    main algorithm

--- main.cpp
+++ main.cpp
@@ -1,31 +1,132 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    // create the window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+enum Direction {
+  UP,
+  RIGHT,
+  DOWN,
+  LEFT,
+};
 
-    // run the program as long as the window is open
-    while (window.isOpen())
-    {
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+class Ant {
+  public:
+    int x, y, windowWidth, windowHeight;
+    Direction dir = UP;
 
-        // clear the window with black color
-        window.clear(sf::Color::Black);
-
-        // draw everything here...
-        // window.draw(...);
-
-        // end the current frame
-        window.display();
+    Ant(int x, int y, int windowWidth, int windowHeight) {
+      this->x = x;
+      this->y = y;
+      this->windowWidth = windowWidth;
+      this->windowHeight = windowHeight;
     }
 
-    return 0;
+    void turnRight() {
+      switch (dir) {
+        case UP:
+          dir = RIGHT;
+          break;
+        case RIGHT:
+          dir = DOWN;
+          break;
+        case DOWN:
+          dir = LEFT;
+          break;
+        case LEFT:
+          dir = UP;
+          break;
+      }
+    }
+
+    void turnLeft() {
+      switch (dir) {
+        case LEFT:
+          dir = DOWN;
+          break;
+        case DOWN:
+          dir = RIGHT;
+          break;
+        case RIGHT:
+          dir = UP;
+          break;
+        case UP:
+          dir = LEFT;
+          break;
+      }
+    }
+
+    void moveForward() {
+      switch (dir) {
+        case UP:
+          y--;
+          break;
+        case RIGHT:
+          x++;
+          break;
+        case DOWN:
+          y++;
+          break;
+        case LEFT:
+          x--;
+          break;
+      }
+
+      if (x > windowWidth) x = 0;
+      if (x < 0) x = windowWidth - 1;
+
+      if (y > windowHeight) y = 0;
+      if (y < 0) y = windowHeight - 1;
+    }
+};
+
+void draw(int[][10] , Ant);
+
+int main() {
+
+  const int width = 400;
+  const int height = 400;
+  int grid[width][height] = {};
+
+  Ant ant = Ant(200, 200, 400, 400);
+
+  grid[ant.x][ant.y] = 1;
+
+  // create the window
+  sf::RenderWindow window(sf::VideoMode(width, height), "My window");
+
+  // run the program as long as the window is open
+  while (window.isOpen())
+  {
+      // check all the window's events that were triggered since the last iteration of the loop
+      sf::Event event;
+      while (window.pollEvent(event))
+      {
+          // "close requested" event: we close the window
+          if (event.type == sf::Event::Closed)
+              window.close();
+      }
+
+      // clear the window with black color
+      window.clear(sf::Color::White);
+
+      // draw everything here...
+      const int state = grid[ant.x][ant.y];
+
+      switch (state) {
+        case 0:
+          ant.turnRight();
+          grid[ant.x][ant.y] = 1;
+          ant.moveForward();
+          break;
+        case 1:
+          ant.turnLeft();
+          grid[ant.x][ant.y] = 0;
+          ant.moveForward();
+          break;
+      }
+
+      // end the current frame
+      window.display();
+  }
+
+  return 0;
 }
+
